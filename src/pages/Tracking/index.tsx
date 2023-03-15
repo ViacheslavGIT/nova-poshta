5e591707edbb9f2d9d3353ba35579cf5cef2372d
Viacheslav <Viacheslav@MacBook-Air-Daria.local>
2023-03-15 21:38:31 +0200

    add logic for input field

--- src/pages/Tracking/index.tsx
+++ src/pages/Tracking/index.tsx
@@ -1,9 +1,55 @@
+import { ChangeEvent, useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
 import "./index.css";
 
 function Tracking() {
+  const [value, setValue] = useState("");
+  const [error, setError] = useState("");
+
+  const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    if (value.length === 0) {
+      setError("");
+    } else if (!value.match(/^\d+$/)) {
+      setError("Please enter numbers only");
+    } else {
+      setError("");
+    }
+    setValue(value);
+  };
+  const handleClick = () => {
+    if (value.length !== 16) {
+      return setError("Please enter 16 symbols");
+    } else if (!value.match(/^\d+$/)) {
+      return setError("Please enter numbers only");
+    }
+
+    if (!error) {
+      const body = {
+        apiKey: "e7df4acb5b2b8512c189a4d185780f9f",
+        modelName: "TrackingDocument",
+        calledMethod: "getStatusDocuments",
+        methodProperties: {
+          Documents: [
+            {
+              DocumentNumber: value ? value : "59000941816430",
+              // Phone: "380939480841", *option
+            },
+          ],
+        },
+      };
+      fetch("https://api.novaposhta.ua/v2.0/json/", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+    }
+  };
   return (
     <div className="wrapper">
       <div className="content-container">
@@ -15,10 +61,15 @@ function Tracking() {
               label="Введіть номер накладної"
               size="small"
               type="search"
+              value={value}
+              onChange={handleChangeInput}
+              error={Boolean(error)}
             />
+            {error ? <span style={{ color: "#d32f2f" }}>{error}</span> : null}
           </div>
+
           <div className="button">
-            <Button fullWidth variant="contained">
+            <Button fullWidth variant="contained" onClick={handleClick}>
               get status
             </Button>
           </div>
